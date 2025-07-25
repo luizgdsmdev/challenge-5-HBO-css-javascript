9f245f9a5d7d66d9c80d924346cfeb887438ef82
Luiz Messias <luizgdsmdev@gmail.com>
2025-07-25 05:26:28 -0300

    Implementation of the top nav, includes mobile interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,11 @@
 
-
+//Control the opening and closing of the navbar on mobile and screen lower than 1280px
 let nav__menu_hamburger_icon = document.getElementById("nav__menu_hamburger_icon");
 
 nav__menu_hamburger_icon.addEventListener("click", () => {
     let nav__menu_options_mobile = document.getElementById("nav__menu_options_mobile");
-    nav__menu_options_mobile.classList.toggle("displayBlockForElement")
+    let nav__menu_hamburger_icon =  document.getElementById("nav__menu_hamburger_icon");
+
+    nav__menu_hamburger_icon.classList.toggle("hamburger_icon_to_x");
+    nav__menu_options_mobile.classList.toggle("nav__menu_options_mobile_open");
 });
