26696a595fce6cc0bb295125e8501ef2c1a37fca
Luiz Messias <luizgdsmdev@gmail.com>
2025-07-28 03:08:52 -0300

    Adding basic structure to plan card

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,23 @@ nav__menu_hamburger_icon.addEventListener("click", () => {
     nav__menu_hamburger_icon.classList.toggle("hamburger_icon_to_x");
     nav__menu_options_mobile.classList.toggle("nav__menu_options_mobile_open");
 });
+
+//Controle the display of the plan
+let content__topPlanPeriodMonth = document.getElementById("content__top-plan-period-month");
+let content__topPlanPeriodAnnual = document.getElementById("content__top-plan-period-annual");
+
+content__topPlanPeriodMonth.addEventListener("click", (event) =>{
+    event.stopImmediatePropagation();
+    console.log("teste")
+
+    content__topPlanPeriodMonth.classList.toggle("plan-focus");
+    content__topPlanPeriodAnnual.classList.toggle("plan-focus");
+});
+
+content__topPlanPeriodAnnual.addEventListener("click", (event) =>{
+    event.stopImmediatePropagation();
+    console.log("teste")
+
+    content__topPlanPeriodMonth.classList.toggle("plan-focus");
+    content__topPlanPeriodAnnual.classList.toggle("plan-focus");
+});
