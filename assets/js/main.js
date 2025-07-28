d72729561d1240b7d9fe1791124d6d1ec378608f
Luiz Messias <luizgdsmdev@gmail.com>
2025-07-28 05:07:17 -0300

    Adding final version for cards price.

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,20 +12,45 @@ nav__menu_hamburger_icon.addEventListener("click", () => {
 
 //Controle the display of the plan
 let content__topPlanPeriodMonth = document.getElementById("content__top-plan-period-month");
+
 let content__topPlanPeriodAnnual = document.getElementById("content__top-plan-period-annual");
+let content__topCardsMonthTitle = document.getElementById("content__top-cards-month-title");
+let annualPriceElementList = document.querySelectorAll(".annual_price");
+let monthPriceElementList = document.querySelectorAll(".month_price");
 
 content__topPlanPeriodMonth.addEventListener("click", (event) =>{
     event.stopImmediatePropagation();
     console.log("teste")
 
-    content__topPlanPeriodMonth.classList.toggle("plan-focus");
-    content__topPlanPeriodAnnual.classList.toggle("plan-focus");
+    content__topPlanPeriodMonth.classList.add("plan-focus");
+    content__topCardsMonthTitle.classList.add("hide_title");
+
+    content__topPlanPeriodAnnual.classList.remove("plan-focus");
+
+    //working on prices
+    monthPriceElementList.forEach((element) => {
+        element.classList.remove("hide_price");
+    });
+    
+    annualPriceElementList.forEach((element) => {
+        element.classList.add("hide_price");
+    });
 });
 
 content__topPlanPeriodAnnual.addEventListener("click", (event) =>{
     event.stopImmediatePropagation();
     console.log("teste")
 
-    content__topPlanPeriodMonth.classList.toggle("plan-focus");
-    content__topPlanPeriodAnnual.classList.toggle("plan-focus");
+    content__topPlanPeriodAnnual.classList.add("plan-focus");
+    content__topPlanPeriodMonth.classList.remove("plan-focus");
+    content__topCardsMonthTitle.classList.remove("hide_title");
+
+    //working on prices
+    annualPriceElementList.forEach((element) => {
+        element.classList.remove("hide_price");
+    });
+
+    monthPriceElementList.forEach((element) => {
+        element.classList.add("hide_price");
+    });
 });
